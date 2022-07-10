fix: broadcast-ed tensor could update grad correctly

chell/core/op.py
@@ -124,20 +124,26 @@ class Operation:
                 "Chell will update grad like doing backward on `node.sum()`")
 
         if depth != 0:
-            o_grad = np.zeros(shape=(1, self.value.size))
+            o_grad = None
             for user in self.users:
                 if user() is not None and user().on_grad_path:
                     for k, user_i in user().inputs.items():
                         if user_i is self:
-                            o_grad += user().prod_jacobian[k]
+                            if o_grad is None:
+                                o_grad = user().prod_jacobian[k].copy()
+                            else:
+                                o_grad += user().prod_jacobian[k]
         else:
             o_grad = np.ones(shape=(1, self.value.size))
 
         if isinstance(self, tensor.Tensor):
+            batch_size = int(o_grad.size / self.value.size)
+            final_shape = [batch_size, *self.value.shape]
+            final_out = o_grad.reshape(final_shape)
             if self.grad is None or not accumulate_grad:
-                self.grad = o_grad.reshape(self.value.shape)
+                self.grad = final_out
             else:
-                self.grad += o_grad.reshape(self.value.shape)
+                self.grad += final_out
 
         else:
             jac = self._jacobian()

chell/core/optimizers/gd.py
@@ -19,9 +19,10 @@ class GD(optimizer.Optimizer):
                 self.velocities[p.node_name]: Dict[str, np.ndarray] = np.zeros(shape=p.value.shape)
 
     def step(self) -> None:
-        for _, p in self.params.items():
+        for node_name, p in self.params.items():
+            mean_grad = p.grad.mean(axis=0, keepdims=True)[0]
             if self.momentum > 0:
-                self.velocities[p.node_name] = self.momentum * self.velocities[p.node_name] + self.lr * p.grad
-                p.value -= self.velocities[p.node_name]
+                self.velocities[node_name] = self.momentum * self.velocities[node_name] + self.lr * mean_grad
+                p.value -= self.velocities[node_name]
             else:
-                p.value -= self.lr * p.grad
+                p.value -= self.lr * mean_grad

test/linear_regression.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from chell.core import tensor
+from chell.core.optimizers import gd
 
 M = 2
 N = 3
@@ -15,6 +16,7 @@ sample_y = np.matmul(true_a, sample_x) + true_b
 
 
 class LinearRegressionTest(unittest.TestCase):
+
     def create_model(self):
         a = tensor.Tensor("a", np.ones((M, N)), requires_grad=True)
         x = tensor.Tensor("x", np.ones((N, 1)))
@@ -23,18 +25,50 @@ class LinearRegressionTest(unittest.TestCase):
 
     def test_direct_train(self):
         model, x, params = self.create_model()
-        for i in range(SAMPLE_COUNT):
-            x.set_value(sample_x[:, [i]])
-            diff = model - sample_y[:, [i]]
+        BATCH_SIZE = 3
+        loss_v = 1
+        count = 0
+        while loss_v > 0.01:
+            i = int(count % int(SAMPLE_COUNT / BATCH_SIZE))
+            rows = list(range(BATCH_SIZE * i, BATCH_SIZE * (i + 1)))
+            x.set_value(sample_x[:, rows])
+            diff = model - sample_y[:, rows]
             diff_square = diff * diff
             loss = diff_square.sum()
             loss.forward()
             loss.backward()
+            loss_v = loss.value[0]
             for p in params:
-                p.value -= p.grad * 0.01
+                grad = p.grad.mean(axis=0, keepdims=True)[0]
+                p.value -= grad * 0.01
+            count += 1
+        print(f"test_direct_train finished with count:{count}")
+        self.assertTrue(loss_v < 0.01)
 
-        self.assertTrue(np.allclose(params[0].value, true_a) and np.allclose(params[1].value, true_b))
-        self.assertTrue(np.allclose(loss.value, 0))
+    def test_optim_gd_train(self, momentum=0.0):
+        model, x, params = self.create_model()
+        optim = gd.GD(params, lr=0.01, momentum=momentum)
+        BATCH_SIZE = 3
+        loss_v = 1
+        count = 0
+        while loss_v > 0.01:
+            i = int(count % int(SAMPLE_COUNT / BATCH_SIZE))
+            rows = list(range(BATCH_SIZE * i, BATCH_SIZE * (i + 1)))
+            x.set_value(sample_x[:, rows])
+            diff = model - sample_y[:, rows]
+            diff_square = diff * diff
+            loss = diff_square.sum()
+            loss.forward()
+            loss.backward()
+            loss_v = loss.value[0]
+            optim.step()
+            count += 1
+
+        print(f"optim_gd_train lr={optim.lr} momentum={optim.momentum} finished with count:{count}")
+        self.assertTrue(loss_v < 0.01)
+
+    def test_optim_gd_train_with_momentum(self):
+        self.test_optim_gd_train(momentum=0.8)
 
 
 if __name__ == "__main__":